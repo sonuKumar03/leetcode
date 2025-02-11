c798f8b3fad0514e8781a18948f16ae8fce66e89
Sonu Kumar <sonukumar03xt@gmail.com>
2025-02-11 23:25:33 +0530

    LC_3174 v2

--- LC_3174.ts
+++ LC_3174.ts
@@ -2,27 +2,6 @@
  * LC - https://leetcode.com/problems/clear-digits/
  * 2025-02-10
  */
-
-function clearDigits(s: string): string {
-  // get the first digit
-
-  const str = s.split("");
-
-  let index = nextDigitIndex(str);
-
-  if (index === -1) return s;
-
-  while (true) {
-    index = nextDigitIndex(str);
-    if (index === -1) break;
-    const char = charLeftToIndex(index, str);
-    if (char === "") break;
-    console.log(index, char);
-    str.splice(index - 1, 2);
-  }
-  console.log(str.join(""));
-  return str.join("");
-}
 function nextDigitIndex(str: string[]): number {
   return str.findIndex((e) => isDigit(e));
 }
@@ -31,15 +10,41 @@ function isDigit(str: string) {
   return !isNaN(Number(str));
 }
 
-function charLeftToIndex(index: number, str: string[]): string {
+function charLeftToIndex(index: number, str: string[]): number {
   for (let i = 0; i < index; i++) {
     if (!isDigit(str[i])) {
-      return str[i];
+      return i;
     }
   }
-  return "";
+  return -1;
 }
 
-const s = "qm93xjkpaaovhqckjhg5j1o4rndtg3bobgeke";
+function clearDigits(s: string): string {
+  // get the first digit
 
-clearDigits(s);
+  const str = s.split("");
+
+  let digitIndex = nextDigitIndex(str);
+
+  if (digitIndex === -1) return s;
+  // map to store indices to delete
+
+  let lastCharIndex = charLeftToIndex(digitIndex, str);
+
+  if (lastCharIndex === -1) return s;
+
+  while (digitIndex < s.length && lastCharIndex >= 0) {
+    const digit = str[digitIndex];
+    const char = str[lastCharIndex];
+    if (isDigit(digit) && !isDigit(char)) {
+      str.splice(lastCharIndex, 1);
+      str.splice(digitIndex, 1);
+      lastCharIndex = -1;
+      digitIndex -= 1;
+    }
+    break;
+  }
+  return str.join("");
+}
+const s = "a3aa4a";
+console.log(clearDigits(s));
